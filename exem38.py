62476c5fb76ba098453f89d089fcefe9a3f81a75
renegadelha <renegadelha@gmail.com>
2024-10-07 21:07:50 -0300

    aula0710

---
+++ exem38.py
@@ -0,0 +1,23 @@
+#estrutura de dados: lista
+#lista corresponde a um vetor (array) de elementos
+
+#lista vazia
+vereadores = list()
+prefeitos = []
+
+#declarar lista preenchida
+lagoa = ['socorro', 'jane', 'flavinho']
+
+#acessando um elementa específico da lista
+lagoa[0]
+
+#adicionando elementos na lista
+lagoa.append('ricardo')
+
+#remover um elemento pelo índice
+lagoa.pop(0)
+
+#remover o elemento pelo seu valor
+lagoa.remove('ricardo')
+
+print(lagoa[0])
---
+++ exem39.py
@@ -0,0 +1,10 @@
+#estrutura de dados: lista
+#lista corresponde a um vetor (array) de elementos
+
+presentes = []
+
+for i in range(4):
+    nome = input('digite seu nome')
+    presentes.append(nome)
+
+print(presentes)
